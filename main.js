49c88c5ac62dedf275d041ab675d54d8201e9511
Tadiwa Chopamba <145779552+AI-DataWizard@users.noreply.github.com>
2024-04-17 20:06:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,11 @@ $(document).ready(function () {
   $("#menu").click(function () {
     $(".navbar").toggleClass("active");
   });
-   $(window).on("scroll load", function () {
+
+  $(window).on("scroll load", function () {
     $("#menu #icon").removeClass("fa-xmark");
     $("#menu #icon").addClass("fa-bars");
-    $(".header .header-2 .navbar").removeClass("active");
+    $(".navbar").removeClass("active");
     i = 0;
-
-    if ($(window).scrollTop() > 68) {
-      $(".header .header-2").addClass("active");
-    } else {
-      $(".header .header-2").removeClass("active");
-    }
-    });
   });
-
+});
